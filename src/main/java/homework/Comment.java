df3cd9db8e49eedd778aea42a80e6a5f1a2e7415
hummai <shining.melon2015@gmail.com>
2020-04-08 16:40:11 +0300

    Added Comment class

---
+++ src/main/java/homework/Comment.java
@@ -0,0 +1,57 @@
+package homework;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "author_name")
+    private String authorName;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    public Comment(String authorName, Post post) {
+        this.authorName = authorName;
+        this.post = post;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", authorName='" + authorName + '\'' +
+                ", post=" + post +
+                '}';
+    }
+}
